3953f974b7b13853d2440a27411ec927dfbf2733
RussellLin <superbr7789@gmail.com>
2022-01-30 21:19:30 +0800

    Add useEffect for CSS animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
@@ -6,13 +6,35 @@ import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext)
+  const [buttonClassIsTriggered, setbuttonClassIsTriggered] = useState(false)
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+  const { items } = cartCtx
+
+  const numberOfCartItems = items.reduce((currentNum, item) => {
     return currentNum + item.amount
   }, 0)
 
+  const buttonClasses = `${classes.button} ${buttonClassIsTriggered ? classes.bump : ''}`
+
+  // add useEffect for utilize the CSS animation
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setbuttonClassIsTriggered(true)
+
+    const timer = setTimeout(() => {
+      setbuttonClassIsTriggered(false)
+    }, 300)
+
+    // return function within useEffect is always seen as clear function for React
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
