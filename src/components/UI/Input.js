e3e1ae70ef0dc83f3fa1359e3a51c6ecbe60fac9
RussellLin <superbr7789@gmail.com>
2022-01-30 16:13:59 +0800

    Make input a custom ref and add useContext for globally get the number of cartItem

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 import classes from './Input.module.css'
 
-const Input = (props) => {
+// *** need add 'React.forwardRef' if want to use custom component as ref ***
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* directly pull out all the key-value pairs of props.input */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   )
-}
+})
 
 export default Input
